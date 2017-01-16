425b3b37dc5494131f7cb31afc476520c35e9994
Flávio Sena <github@sounoob.com.br>
2017-01-16 21:57:23 -0200

    Delete index.php

--- index.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-include 'slugify.php';
-
-$string = 'Esse é o texto a ser trabalhado.          deixei espaço de propósito
- uma quebra de linha aqui';
-
-echo slugify($string);//esse-e-o-texto-a-ser-trabalhado-deixei-espaco-de-proposito-uma-quebra-de-linha-aqui
